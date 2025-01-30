e3446409da7723f80cf010f274af11451f131c96
Dale Lane <dale.lane@uk.ibm.com>
2025-01-30 15:38:30 +0000

    chore: update dependencies
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- scripts/produce-random.sh
+++ scripts/produce-random.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp ./target/sample-kafka-apps-0.0.4-jar-with-dependencies.jar com.ibm.eventautomation.demos.producers.RandomBytesProducer
+java -cp ./target/sample-kafka-apps-0.0.5-jar-with-dependencies.jar com.ibm.eventautomation.demos.producers.RandomBytesProducer
