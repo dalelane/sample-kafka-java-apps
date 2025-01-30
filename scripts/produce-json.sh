1cca7ddef854f3051ecbaea34178a84761d3f28c
Dale Lane <dale.lane@uk.ibm.com>
2025-01-30 19:34:39 +0000

    chore: reformatting scripts
    Signed-off-by: Dale Lane <dale.lane@uk.ibm.com>

--- scripts/produce-json.sh
+++ scripts/produce-json.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-java -cp ./target/sample-kafka-apps-0.0.5-jar-with-dependencies.jar com.ibm.eventautomation.demos.producers.JsonProducer
+
+java \
+    -cp ./target/sample-kafka-apps-0.0.5-jar-with-dependencies.jar \
+    com.ibm.eventautomation.demos.producers.JsonProducer
